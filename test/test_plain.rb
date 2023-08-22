5d42772495f94cae5ef2b9b56742a73dfa100732
Yegor Bugayenko <yegor256@gmail.com>
2023-08-22 21:28:22 +0300

    #26 strike

--- test/test_plain.rb
+++ test/test_plain.rb
@@ -33,6 +33,7 @@ class GptTranslate::PlainTest < Minitest::Test
   def test_simple_map
     assert_equal('Hello, world!', GptTranslate::Plain.new('Hello, **world**!').to_s)
     assert_equal('Hello, Jeff!', GptTranslate::Plain.new('Hello, _Jeff_!').to_s)
+    # assert_equal('Hello, Walter!', GptTranslate::Plain.new('Hello, ~Walter~!').to_s)
     assert_equal("Hi\n\nBye", GptTranslate::Plain.new("  Hi\n\nBye\n\n\n").to_s)
     assert_equal('Hi, dude!', GptTranslate::Plain.new("  Hi,\ndude!\n").to_s)
   end
